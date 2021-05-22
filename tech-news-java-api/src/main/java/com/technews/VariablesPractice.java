b102832df58b9f92454932333777542a30f03be3
Carlicia Smith <smith.carlicia@gmail.com>
2021-05-22 10:32:25 -0400

    structured database

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 5.0;
+
+        String c = "cat";
+
+        double sum = a + b;
+        String concat = a + c;
+
+        System.out.println(sum);
+        System.out.println(concat);
+
+    }
+}
